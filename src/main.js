dd706a010b17ebcaf6cba936183b0d509a086eef
Henry Eschenmoser <henry.eschenmoser@gmail.com>
2025-12-19 18:08:49 +0100

    Implemented playing and reset logic, added current player as text

--- src/main.js
+++ src/main.js
@@ -1,22 +1,59 @@
 import { createInitialState } from "./state.js";
 
 const state = createInitialState();
-showBoard(state);
+const boardEl = document.querySelector(".board");
+const resetBtn = document.querySelector("#reset-btn");
+const statusEl = document.querySelector("#status");
+
+boardEl.addEventListener("click", (event) => {
+    // Feld finden, welches geklickt wurde
+    const field = event.target.closest(".field");
+    if (!field) return;
+
+    const col = parseInt(field.dataset.col);
+
+    for (let row = 5; row >= 0; row--) {
+        if (state.board[row][col] === "") {
+            state.board[row][col] = state.current; // Aktualisieren das Board mit dem aktuellen Spieler
+
+            state.current = state.current === 'r' ? 'b' : 'r';
+
+            showBoard(state);
+            break;
+        }
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+    state.board = state.board.map(row => row.fill(""));
+    state.current = 'r';
+
+    showBoard(state);
+});
 
 function showBoard(state) {
-    const boardEl = document.querySelector(".board");
     boardEl.innerHTML = ""; // alte Felder löschen
+    const playerName = state.current === 'r' ? 'Rot' : 'Blau';
+    statusEl.textContent = `${playerName} ist am Zug`;
 
     for (let row = 0; row < 6; row++) {
-        for (let column =0; column < 7; column++) {
+        for (let col =0; col < 7; col++) {
             const field = document.createElement("div");
             field.className = "field";
 
-            // Position für spätere Klick-Logik merken
-            field.dataset.row = row;
-            field.dataset.column = column;
+            const type = state.board[row][col]; // '' | 'r' | 'b'
+            if (type !== "") {
+                const piece = document.createElement("div");
+                piece.className = `piece ${type === "r" ? "red" : "blue"}`;
+                field.appendChild(piece);
+            }
 
+            // Position für spätere Klick-Logik merken
+            field.dataset.row = row; // Als String abgespeichert
+            field.dataset.col = col; // Als String abgespeichert
             boardEl.appendChild(field);
         }
     }
 }
+
+showBoard(state);
