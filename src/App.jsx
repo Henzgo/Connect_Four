6e78e1e27cc4de98e777f6242968d9cadbafe7a0
Henry Eschenmoser <henry.eschenmoser@gmail.com>
2025-12-31 18:19:34 +0100

    Implement hitbox and piece setting logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useGameState } from "./logic/useGameState";
 import { Status } from "./components/Status";
-import { Board2D } from "./components/Board2D";
+import { Board } from "./Board";
 import "./App.css";
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei";
 
 export default function App() {
   const { board,
@@ -18,7 +20,14 @@ export default function App() {
 
       <Status current={current} winner={winner} />
 
-      <Board2D board={board} onFieldClick={handleFieldClick} />
+      <Canvas camera={{ position: [0, 2, 10]}}>
+        <color attach="background" args={['white']} />
+        <Board board={board} onFieldClick={handleFieldClick}/>
+        <ambientLight intensity={0.5}/>
+        <pointLight position={[10, 10, 10]} intensity={1}/>
+
+        <OrbitControls />
+      </Canvas>
       
       <div className="controls">
         <button onClick={handleUndo} className="undo">Undo</button>
